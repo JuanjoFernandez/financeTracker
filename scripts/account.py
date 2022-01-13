9f9e983e0102b93c0ef49bf7e5c6645b837884b1
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-01-13 14:02:40 -0600

    Adds queries to account creation

--- scripts/account.py
+++ scripts/account.py
@@ -1,18 +1,20 @@
-from unicodedata import name
-
+import sqlite3
+import os
 
 class Account:
-    """"""
-    def __init__(self, id, name, institution, date_opened, type, balance, limit) -> None:
-        self.id = id
-        self.name = name
-        self.institution = institution
-        self.date_opened = date_opened
-        self.type = type
-        self.balance = balance
-        self.limit = limit
+    """
+    Expects an array of parameters according to the number of columns in the
+    "accounts" database.
+
+    """
+    
+    # Class variables
+    
+    def __init__(self, account_info) -> None:
+        self.account_info = account_info
 
     def create(self):
+        
         pass
 
     def modify(self):
@@ -21,3 +23,20 @@ class Account:
     def update_balance(self, amount):
         pass
         
+path = os.path.join('databases', 'accounts.db')
+connection = sqlite3.connect(path)
+cursor = connection.cursor()
+
+# Getting the next id
+query = 'SELECT id FROM accounts WHERE id=(SELECT max(id) FROM accounts);'
+cursor.execute(query)
+id_array = cursor.fetchone()
+current_id = id_array[0] + 1
+
+# Getting number of columns
+query = "PRAGMA table_info('accounts');"
+cursor.execute(query)
+columns_info = cursor.fetchall()
+column_number = len(columns_info)
+
+connection.close
