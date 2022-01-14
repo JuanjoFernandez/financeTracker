dbfa73728c0bddd23335ec696add5dc975c80dbf
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-01-14 09:14:01 -0600

    Gets new account information

--- scripts/account.py
+++ scripts/account.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-
+from datetime import datetime as dt
 class Account:
     """
     Expects an array of parameters according to the number of columns in the
@@ -23,6 +23,7 @@ class Account:
     def update_balance(self, amount):
         pass
         
+# Database parameters
 path = os.path.join('databases', 'accounts.db')
 connection = sqlite3.connect(path)
 cursor = connection.cursor()
@@ -33,10 +34,22 @@ cursor.execute(query)
 id_array = cursor.fetchone()
 current_id = id_array[0] + 1
 
-# Getting number of columns
+# Getting number and name of columns
 query = "PRAGMA table_info('accounts');"
 cursor.execute(query)
 columns_info = cursor.fetchall()
 column_number = len(columns_info)
 
+# Getting the account information
+account_info = []
+for item in range(column_number):
+    if columns_info[item][1] == 'id':
+        print (f"The id for the new account will be: {current_id}")
+        account_info.append(current_id)
+    
+    else:
+        account_info.append(\
+        input(f"Please provide account {columns_info[item][1]}, expects a(n) {columns_info[item][2]}:"))
+
+print (account_info)
 connection.close
