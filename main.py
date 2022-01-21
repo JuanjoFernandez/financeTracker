c43baae91675f4fae4868e7a054e88911b74fcab
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-01-21 11:17:18 -0600

    Adds API

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask.views import MethodView
+
+app = Flask(__name__)
+
+class HomePage(MethodView):
+    
+    def get(self):
+        return render_template("index.html")
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+
+if __name__ == "__main__":
+    app.run(debug = True)
