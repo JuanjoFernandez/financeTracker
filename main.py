41a0c59b7c498cd0d82c9b340ebe9f4c082accae
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-01-31 11:47:33 -0600

    Adds new route

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ class HomePage(MethodView):
     def get(self):
         return render_template("index.html")
 
+class NewOperation(MethodView):
+
+    def get(self):
+        return render_template("new.html")
+
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/new', view_func=NewOperation.as_view('new'))
 
 if __name__ == "__main__":
     app.run(debug = True)
