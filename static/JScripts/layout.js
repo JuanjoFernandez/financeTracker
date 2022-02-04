75fe868913335176dc3c4e061088b70b6b8a2999
Juan.J.Diaz BairesDev <juanjo.fde@gmail.com>
2022-02-04 09:31:09 -0600

    Templates the layout

---
+++ static/JScripts/layout.js
@@ -0,0 +1,5 @@
+let newButton = document.getElementById("new");
+let accountButton = document.getElementById("account")
+
+newButton.addEventListener("click", console.log("new"));
+accountButton.addEventListener("click", console.log("account"));
